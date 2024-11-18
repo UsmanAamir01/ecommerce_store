3727c8b020ab52a78f2cb3bdfa55bce9483c0777
UsmanAamir01 <f223709@cfd.nu.edu.pk>
2024-11-18 12:25:31 +0500

    Database Connectivity

---
+++ ecommerce/database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ecommerce
+{
+    class Database
+    {
+
+        public OracleConnection GetConnection()
+        {
+            string connectionstring = "User Id= Ecomdb; Password = 37093688; Data Source=//localhost:1521/XE";
+            return new OracleConnection(connectionstring);
+        }
+    }
+
+}
